ce37fdaa277f503374afef9b3e3f5ec2a843a9ff
mdkaifsiddique <mdkaifstatistics@gmail.com>
2025-06-25 23:03:24 +0530

    first commit

---
+++ 00_temap/0_temp.py
@@ -0,0 +1,14 @@
+# Comment
+
+print(786) #print statement
+
+first_name = 'Kaif'
+last_name = 'Siddique'
+age = 20
+marital_status = False
+
+# print the first name & last name with age and marital status
+
+print(first_name+' '+last_name, age, marital_status)
+
+# print(f'my name is {first_name}{last_name} and i am {age} year old, and my marital_status is {marital_status}')
